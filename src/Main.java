8ff42c1bcc605dbd3e151d6f5f27a99cf6bfd65e
tangy-depth1 <SergeyVasynin2008@gmail.com>
2026-02-12 22:00:09 +0400

    added additional tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount("Аккаунт 1");
+        BankAccount acc2 = new BankAccount("Аккаунт 2");
 
+        acc1.setBalance(1000);
+        acc2.setBalance(500);
+
+        System.out.println("Баланс до: acc1=" + acc1.getBalance() + ", acc2=" + acc2.getBalance());
+        boolean result = acc1.transfer(acc2, 200);
+        System.out.println("Перевод выполнен: " + result);
+        System.out.println("Баланс после: acc1=" + acc1.getBalance() + ", acc2=" + acc2.getBalance());
+
+        result = acc1.transfer(acc1, 100);
+        System.out.println("Перевод выполнен: " + result);
+
+        result = acc1.transfer(acc2, 5000);
+        System.out.println("Перевод выполнен: " + result);
+
+        System.out.println("Они равны:"+acc1.equals(acc2));
+        System.out.println("Они равны:"+acc1.equals(acc1));
     }
 }
